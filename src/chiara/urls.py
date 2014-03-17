0002906715e9484d2e0160c16eee45211a95deca
dominikreis <dominik.reis@gmail.com>
2014-03-17 14:15:08 +0100

    Add base of the reStructuredText documentation.

--- src/chiara/urls.py
+++ src/chiara/urls.py
@@ -1,5 +1,6 @@
+import os
+from chiara.settings.common import SITE_ROOT
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -18,4 +19,7 @@ urlpatterns = patterns('',
     url(r'^my_shared_folder/', 'collection.views.my_shared_folder'),
     url(r'^retrieve_new_collections/', 'collection.views.retrieve_new_collections'),
     url(r'^manage_my_collections/', 'collection.views.manage_my_collections'),
+    
+    url(r'^documentation/(?P<path>.*)$', 'django.views.static.serve', \
+        {'document_root':os.path.join(SITE_ROOT, 'documentation/build/html')}),
 )
---
+++ src/documentation/build/_sources/index.txt
@@ -0,0 +1,22 @@
+.. Chiara documentation master file, created by
+   sphinx-quickstart on Mon Mar 17 12:29:40 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Chiara's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
