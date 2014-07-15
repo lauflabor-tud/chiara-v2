0a6b59021eb5610ec6fd62be0b190260b471372f
Dominik Reis <dominik.reis@gmail.com>
2014-07-15 11:04:48 +0200

    Change timezone to Europe/Berlin

--- src/chiara/settings/common.py
+++ src/chiara/settings/common.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'chiara.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
